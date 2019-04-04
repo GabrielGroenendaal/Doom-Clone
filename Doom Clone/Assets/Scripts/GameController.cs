81cc1f684d8bb749b2e9167779c87cbd01b27c06
Gabriel Groenendaal <gfg243@nyu.edu>
2019-04-04 10:53:17 -0400

    Updte to fix Ignore

---
+++ Doom Clone/Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This is the brain, the nervous system 
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
